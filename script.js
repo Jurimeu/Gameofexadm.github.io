4065507309ec91ea4cefbc9d8244a04f12891e31
Jurimeu <139811045+Jurimeu@users.noreply.github.com>
2023-10-02 14:14:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,88 @@
+var diryJ, dirxJ, jog, velJ, posX, posY;
+var tamTelaW, tamTelaH;
+var jogo;
+var frames;
 
+
+
+//Função que determina os eventos quando eu APERTO as teclas.
+function teclaDw() {
+    /*"event.keyCode; Obtém o valor Unicode da tecla do teclado pressionada:*/
+    var tecla = event.keyCode;
+    if (tecla == 38) {//Cima
+        //Observar que o "eixo Y é invertido", no caso, para cima é negativo
+        diryJ = -1;
+    }
+    else if (tecla == 40) {//Baixo
+        diryJ = +1;
+    }
+    if (tecla == 37) {//Esquerda
+        dirxJ = -1;
+    }
+    else if (tecla == 39) {//Direita
+        dirxJ = +1;
+    }
+    if (tecla == 32) {//TIRO
+
+    }
+
+}
+
+//Função que determina os eventos quando eu SOLTO as teclas.
+function teclaUp() {
+    var tecla = event.keyCode;
+    if ((tecla == 38) || (tecla == 40)) {//Cima ou Baixo
+        diryJ = 0;
+    }
+    if ((tecla == 37) || (tecla == 39)) {//Esquerda ou Direita
+        dirxJ = 0;
+    }
+
+}
+
+function controlaJogador(){
+    posY += diryJ*velJ;
+    posX += dirxJ*velJ;
+
+    //retorna a posição de um elemento especificado.
+    jog.style.top = posY + "px";
+    jog.style.left = posX + "px";
+}
+function gameLoop() {
+    //"If "jogo" for TRUE...
+    if(jogo === true){
+        //Funções de Controle
+        controlaJogador();
+    }
+    //Função que vai gerir o Loop do game, gerando a animação - OBSERVE A RECURSIVIDADE (gameLoop -> frames -> gameLoop)
+    frames = requestAnimationFrame(gameLoop);
+}
+
+//Função que organiza a inicialização do jogo
+function inicia() {
+    jogo =true;
+    //Inicialização da tela:
+    //A propriedade innerHeight retorna a largura da área de conteúdo de uma janela
+    tamTelaH = window.innerHeight;
+    tamTelaW = window.innerWidth;
+
+    //Inicialização Jogador:
+    dirxJ = diryJ = 0;
+    posX = tamTelaW/2;
+    posY = tamTelaH/2;
+    velJ = 5;
+    jog = document.getElementById("naveJog");
+    //retorna a posição de um elemento especificado.
+    jog.style.top = posY + "px";
+    jog.style.left = posX + "px";
+    gameLoop();
+
+
+}
+
+//addEventListener() registra uma única espera de evento em um único alvo(no caso, window).
+//alvo.addEventListener(tipo, escuta[, usarCaptura]);
+window.addEventListener("load", inicia);
+
+document.addEventListener("keydown", teclaDw);
+document.addEventListener("keyup", teclaUp);
